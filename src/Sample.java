496d4d12a0e4c7d36cb770930ee0d6c2d492a305
Kyle Williams <kjwkc3@mail.umsl.edu>
2016-04-11 03:37:06 -0500

    General formatting and clarification on some variables.
    Throwing an outOfBounds exception when ran, but displays the proper
    output. Will need to look at further.

--- src/Sample.java
+++ src/Sample.java
@@ -4,36 +4,16 @@ import java.util.List;
 
 
 // Data values by pos:
-// 0: Alternate location? 			{ 0 = false, 1 = true }
-// 1: Is there a bar? 				{ 0 = false; 1 = true }
-// 2: Is it a Friday or Saturday?	{ 0 = false; 1 = true }
-// 3: Am I very hungry?				{ 0 = false; 1 = true }
-// 4: How many patrons?				{ 0 = none; 1 = some; 2 = full }
-// 5: Price range?					{ 0 = $; 1 = $$; 2 = $$$ }
-// 6: Is it raining?				{ 0 = false; 1 = true;
-// 7: Do I have a reservation?		{ 0 = false; 1 = true;
-// 8: Type of food?					{ 0 = French; 1 = Thai; 2 = Burger; 3 = Italian }
-// 9: Estimated wait time?			{ 0 = 0-10; 1 = 10-30; 2 = 30-60; 3 = >60 }
-
-
-//None = 0
-//Some = 1
-//Full = 2
-
-//$   = 0
-//$$  = 1
-//$$$ = 2
-
-//French  = 0
-//Thai    = 1
-//Burger  = 2
-//Italian = 3
-
-//0-10  = 0
-//10-30 = 1
-//30-60 = 2
-//> 60  = 3
-
+// 0: Alternate location? 			{ 0 = false, 	1 = true 							}
+// 1: Is there a bar? 				{ 0 = false; 	1 = true 							}
+// 2: Is it a Friday or Saturday?	{ 0 = false; 	1 = true 							}
+// 3: Am I very hungry?				{ 0 = false; 	1 = true 							}
+// 4: How many patrons?				{ 0 = none; 	1 = some; 	2 = full 				}
+// 5: Price range?					{ 0 = $; 		1 = $$; 	2 = $$$ 				}
+// 6: Is it raining?				{ 0 = false; 	1 = true; 							}
+// 7: Do I have a reservation?		{ 0 = false; 	1 = true;							}
+// 8: Type of food?					{ 0 = French; 	1 = Thai; 	2 = Burger; 3 = Italian }
+// 9: Estimated wait time?			{ 0 = 0-10; 	1 = 10-30; 	2 = 30-60; 	3 = >60 	}
 
 public class Sample {
 	FileHandler fh = null;;
@@ -45,8 +25,21 @@ public class Sample {
 	public Sample(FileHandler fh) {
 		this.fh = fh;
 		fileData = fh.linesInFile;
-		numDecisionVariables = Integer.valueOf(fh.getWordAt(fileData[0], 0)); 
-		totalData = Integer.valueOf(fh.getWordAt(fileData[0], 1));
+		
+		// The "+1" is because of the way the example is laid out in the 
+		// project description. numDecisionVariables is just that - it does
+		// not include the actual classification, hence why the "+1" is
+		// needed to get that variable. There may be indexing errors if
+		// the example in the project description was meant to include the
+		// classification originally. In that case it should be easily
+		// fixed by removing the "+1".
+		numDecisionVariables = Integer.valueOf(fh.getWordAt(fileData[0], 0)) + 1;
+		
+		// The "-1" is similar to the above. For some reason the total data 
+		// value in the project description includes the first line that gives 
+		// the number of decision values and total data, so the actual data is 
+		// one less than the value given.
+		totalData = Integer.valueOf(fh.getWordAt(fileData[0], 1)) - 1;
 		
 		createData();
 	}
@@ -55,6 +48,7 @@ public class Sample {
 		for (int i = 1; i < totalData; i++) { // elem 1 is first instance of actual data
 			HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
 			String curLine = fileData[i];
+			
 			
 			for (int j = 0; j < numDecisionVariables; j++) {
 				int dataValue = Integer.valueOf(fh.getWordAt(curLine, j));
@@ -69,7 +63,7 @@ public class Sample {
 	public void displayData() {
 		System.out.println("Data: ");
 		
-		for (int i = 0; i < totalData - 1; i++) {
+		for (int i = 0; i < totalData; i++) {
 			System.out.println(i + ":\t" + data.get(i));
 		}
 	}
